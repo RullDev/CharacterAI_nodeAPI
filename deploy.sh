3fba40d01f2501bc591f14f64e5bc04bd573d77c
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-09-28 23:34:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,5 @@ npm run build
 node index.js
 
 apt update && apt install chromium-browser
+
+node -p "console.log(process.env.PUPPETEER_CACHE_DIR || require('puppeteer').default.executablePath().split('/').slice(0, -2).join('/'))"
